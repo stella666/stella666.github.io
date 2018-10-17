db03f8dc2f910c84f247570310a2463c825e5e49
stella666 <arnystella7@gmail.com>
2018-10-17 11:56:48 +0000

    listaverk 2

---
+++ P1b/sketch.js
@@ -0,0 +1,11 @@
+function setup(){
+    createCanvas(564,735);
+    background(232,215,171)
+    stroke(105,90,31)
+    line(300,0,300,192);
+    noFill();
+    curve(4,-200,200,5,400,5,600,-200);
+    line(200,5,200,70);
+    line(400,5,400,140)
+    line(100,70,300,70)
+}// Hér kemur kóðinn þinn:
