633933c6fe297e95f6e6b20722c4182aa679898e
stella666 <arnystella7@gmail.com>
2018-10-17 08:56:00 +0000

    100

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -2,7 +2,7 @@ function setup(){
     createCanvas(300,300);
     background(255,90,0);
     fill(154);
-    ellipse(100,100,50,50);
+    ellipse(100,100,100,100);
     fill(0);
     textSize(20);
     text("Halló geimur",200,40);
