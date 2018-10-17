68088533579cc4c44bafc5d97c295171810c5d8d
stella666 <arnystella7@gmail.com>
2018-10-17 09:16:38 +0000

    litir

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -3,14 +3,10 @@ function setup(){
     background(255,90,0);
     fill(154);
     ellipse(100,100,100,100);
-    fill(0,90,200);
+    fill('#fae');
     textSize(20);
     text("Halló geimur",120,40);
-<<<<<<< HEAD
     rect(100,100,10,10);
+    fill(255, 204, 0);
     ellipse(100,200,50,50);
-=======
-    rect(200,200,10,10);
-    ellise(100,200,50,50);
->>>>>>> 9e3d73a4cc68ac91a353a7e130a894fb06ccaf46
 }// Hér kemur kóðinn þinn:
