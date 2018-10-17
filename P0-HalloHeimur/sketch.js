9404708440059c29b40575988cb9a51adf8b1502
stella666 <arnystella7@gmail.com>
2018-10-17 08:49:47 +0000

    up

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,1 +1,9 @@
-// Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(300,300);
+    background(255,0,0);
+    fill(150);
+    ellipse(100,100,50,50);
+    fill(0);
+    textSize(20);
+    text("Halló heimur",200,40);
+}// Hér kemur kóðinn þinn:
