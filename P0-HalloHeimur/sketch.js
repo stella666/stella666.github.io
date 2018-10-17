4823075e3f3c06f1e27d091b299eee6ccd58899a
stella666 <arnystella7@gmail.com>
2018-10-17 09:06:26 +0000

    laga villu

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -6,6 +6,6 @@ function setup(){
     fill(0,90,200);
     textSize(20);
     text("Halló geimur",120,40);
-    rect(200,200,10)
-    ellise(100,200,50,50);
+    rect(100,100,10,10);
+    ellipse(100,200,50,50);
 }// Hér kemur kóðinn þinn:
