0d3fa0d3f7582bbbdfed2af981ba8da441a7f73d
stella666 <arnystella7@gmail.com>
2018-10-31 10:10:03 +0000

    up

--- P3a/sketch.js
+++ P3a/sketch.js
@@ -1,3 +1,5 @@
+var h =0;
+
 function setup() {
   createCanvas(400,400);
   fill(0);
@@ -8,11 +10,10 @@ function setup() {
 function draw(){
   fill(255,0,128);
   background(255, 215, 0);
-    for(var x = 0; x < width; x = x+10){
       for(var y = 0; y < height; y = y+1){
+        
         for(var i = 0; i < random(1,13) ; i = i + 1) {
-        ellipse(15 + i*30, 15 + y*31, 10, 10);
+        ellipse(15 + i*25, 15 + y*31, 10, 10);
       }
     }
-  }
 }
